36ec95611e04c0e2ea6099190a0313dce5dd8fe2
Jasper Grant <jasperbengrant@gmail.com>
2024-02-16 15:19:14 -0400

    Added start of FSM in Q1

--- A1/Q1.py
+++ A1/Q1.py
@@ -9,22 +9,30 @@ from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_D
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 # Sensor types
 from ev3dev2.sensor.lego import ColorSensor, GyroSensor
+# Button
+from ev3dev2.button import Button
 
 # Set up robot as tank along with sensors
 robot = MoveTank(OUTPUT_A, OUTPUT_D)
 robot._gyro = GyroSensor(address = INPUT_2)
 robot._cs = ColorSensor(address = INPUT_1)
 
-# Functiom to dead reckon straight distance cm
-def dead_reckon_straight(distance):
+# Set up buttons
+button = Button()
+
+state = "Line Following"
+
+def follow_line():
     pass
 
-# Function to dead reckon turn angle degrees
-def dead_reckon_turn(angle):
+def find_line():
     pass
 
-
-while(1):
-    robot.on(left_speed=10, right_speed=10)
-
-
+while not button.any():
+    if state == "Line Following":
+        follow_line()
+    elif state == "Line Finding":
+        find_line()
+    else:
+        print("Invalid state")
+        exit(-1)
