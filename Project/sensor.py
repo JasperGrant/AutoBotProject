09170a04f50777214a4ae87ee898ae472fb0b378
Jasper Grant <jasperbengrant@gmail.com>
2024-04-11 10:55:49 -0300

    Added corners to sensor side

--- Project/sensor.py
+++ Project/sensor.py
@@ -5,7 +5,7 @@
 # 2024-04-06
 
 from time import sleep
-from math import cos, sin, radians
+from math import cos, sin, radians, degrees
 import random
 
 # Sensor inputs
@@ -21,6 +21,14 @@ from ev3dev2.motor import (
 
 servo = MediumMotor(OUTPUT_C)
 ultrasonic_sensor = UltrasonicSensor(address=INPUT_3)
+
+# Initialize global point map
+point_map = [[], [], [], []]
+
+# Six feet in cm
+SIX_FEET = 182.88
+# Limit for how far a line candidate can be from the expected position of the line
+NEAREST_NEIGHBOUR_LIMIT = 10
 
 
 def reset_servo():
@@ -62,7 +70,7 @@ def sensor_scan(width, resolution, robot_pose):
 
 def cardinal_direction_sensor_scan(width, resolution, robot_pose):
     # Init empty lists
-    points = [[], [], [], []]
+    global point_map
     # Cardinal directions Right, Up, Left, Down
     cardinal_directions = ["R", "U", "L", "D"]
     # Loop through integers 0-3 and each cardinal direction
@@ -71,19 +79,88 @@ def cardinal_direction_sensor_scan(width, resolution, robot_pose):
         start = (i * 90) - width // 2
         end = (i * 90) + width // 2
         for angle in range(start, end, resolution):
-            move_servo_to_angle(angle + robot_pose[2])
+            move_servo_to_angle(angle + degrees(robot_pose[2]))
             distance = get_ultrasonic_distance()
             if distance == 255:
                 continue
             map_file = open("map.txt", "a")
             point = transform_distance_to_coordinate(
-                distance, -angle + robot_pose[2], robot_pose
+                distance, -angle + degrees(robot_pose[2]), robot_pose
             )
-            points[i].append(point)
+            point_map[i].append(point)
             map_file.write(direction + "," + str(point[0]) + "," + str(point[1]) + "\n")
             map_file.close()
             sleep(0.2)
-    return points
+
+
+# Function to get vertical line based on mode of points
+def get_vertical_line(points, line):
+    # Convert x values of points to integers
+    x = [round(point[0]) for point in points]
+    # Get the mode of the x values
+    line_locations = sorted(set(x), key=x.count)
+    line_location = line_locations[-1]
+    # Set limit based on line position
+    if line == "L":
+        limit = 0
+    else:
+        limit = SIX_FEET
+    # If selected point is too far from nearest neighbour, remove it
+    while abs(line_location - limit) > NEAREST_NEIGHBOUR_LIMIT:
+        line_locations.pop()
+        if line_locations == []:
+            raise ValueError(f"No valid line candiates for {line} line.")
+        line_location = line_locations[-1]
+    # Return final line candidate as two points
+    return [(line_location + 0.5, 0), (line_location + 0.5, SIX_FEET)]
+
+
+# Function to get vertical line based on mode of points
+def get_horizontal_line(points, line):
+    # Convert y values of points to integers
+    y = [round(point[1]) for point in points]
+    # Get the mode of the y values
+    line_locations = sorted(set(y), key=y.count)
+    line_location = line_locations[-1]
+    # Set limit based on line position
+    if line == "U":
+        limit = SIX_FEET
+    else:
+        limit = 0
+    # If selected point is too far from nearest neighbour, remove it
+    while abs(line_location - limit) > NEAREST_NEIGHBOUR_LIMIT:
+        print(line_location)
+        line_locations.pop()
+        if line_locations == []:
+            raise ValueError(f"No valid line candiates for {line} line.")
+        line_location = line_locations[-1]
+    # Return final line candidate as two points
+    return [(0, line_location + 0.5), (SIX_FEET, line_location + 0.5)]
+
+
+# Function to identify walls based on four groups of points
+def wall_identification(data):
+    walls = []
+    # Split data into four groups
+    data = [[(float(point[0]), float(point[1])) for point in group] for group in data]
+    # Get vertical and horizontal lines for each group
+    walls.append(get_vertical_line(data[0], "R"))
+    walls.append(get_horizontal_line(data[1], "U"))
+    walls.append(get_vertical_line(data[2], "L"))
+    walls.append(get_horizontal_line(data[3], "D"))
+    map_file = open("map.txt", "a")
+    corners = [
+        (walls[0][0], walls[1][1]),
+        (walls[0][0], walls[3][1]),
+        (walls[2][0], walls[3][1]),
+        (walls[2][0], walls[1][1]),
+    ]
+    point_map[4] = corners
+    for point in corners:
+        map_file.write("C," + str(point[0]) + "," + str(point[1]) + "\n")
+    map_file.close()
+
+    return walls
 
 
 # Test the sensor scan function
